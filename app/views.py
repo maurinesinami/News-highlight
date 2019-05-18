f4763f4157b6a142395c60aee7f34e57ed7a8c6a
maurinenami <maurinenami>
2019-05-18 13:16:56 +0300

    create a variable in views.py

--- app/views.py
+++ app/views.py
@@ -8,4 +8,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'news-highlights'
+    return render_template('index.html',messag = message)
