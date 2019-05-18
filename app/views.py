c26ae5515e4863278b985b508e6d5d35ab555d31
maurinenami <maurinenami>
2019-05-18 13:41:11 +0300

    created an instance folder with a new config.py file in it

--- app/views.py
+++ app/views.py
@@ -8,10 +8,10 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'news-highlights'
-    return render_template('index.html',messag = message)
-    @app.route('/news/<int:news_id>')
-    def news(news_id):
-        '''view news page function that returns the movie details page and its data
-        '''
-        return render_template('news.html',id = news_id)
+    title = 'news-highlights'
+    return render_template('index.html',title = title)
+@app.route('/news/<int:news_id>')
+def news(news_id):
+    '''view news page function that returns the movie details page and its data
+    '''
+    return render_template('news.html',id = news_id)
